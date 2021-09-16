b6f37ef0e1e062234e22789eb55e7620433dfded
Toby Twigger <tobytwigger1@gmail.com>
2021-09-16 04:09:29 +0100

    Initial working Vue app

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from "vue";
+import App from "./App.vue";
+import UiKit from "@bristol-su/portal-ui-kit";
+import "./utils.css";
+
+Vue.config.productionTip = false;
+
+Vue.use(UiKit, {});
+new Vue({
+    render: (h) => h(App),
+}).$mount("#app");
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    presets: [
+        require("@bristol-su/portal-ui-kit/src/install/tailwind.config.js"),
+    ],
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
